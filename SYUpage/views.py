945c2547f58908d1a473deb4e12e88e9dd46aefc
JunEon <junun24@gmail.com>
2023-02-27 22:11:46 +0900

    지원서 리스트 접근 제한 수정

--- SYUpage/views.py
+++ SYUpage/views.py
@@ -1,16 +1,15 @@
 from .models import JungBo
 from .serializers import JungBoSerializer
-from rest_framework import viewsets, permissions
-from django.shortcuts import redirect, render
-from rest_framework.permissions import AllowAny
+from rest_framework import viewsets
+from .permissions import UserPermission
 
 # JungBo의 목록, detail 보여주기, 수정하기, 삭제하기 모두 가능
 class JungBoViewSet(viewsets.ModelViewSet):
-    permission_classes = [
-         permissions.AllowAny
-    ]
-    queryset = JungBo.objects.all()
-    serializer_class = JungBoSerializer
+     permission_classes = [
+          UserPermission
+     ]
+     queryset = JungBo.objects.all()
+     serializer_class = JungBoSerializer
 
 
     
